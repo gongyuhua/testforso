552819adab017d7011ad8e5a3c7e285e709d0820
gongyuhua <yg348@njit.edu>
2018-06-27 11:20:27 -0400

    Procfile for Heroku

---
+++ config/services.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => env('SES_REGION', 'us-east-1'),
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+    'google' => [
+        'client_id' => '818762209938-fm0f5pk2nvrluokjbun03tpls9iohnp7.apps.googleusercontent.com',         // Google Client ID
+        'client_secret' => 'dBMgJwt2D4-dofHvR__6L_GW', //  Google Client Secret
+        'redirect' => 'http://127.0.0.1:8000/login/google/callback',
+    ],
+
+];
