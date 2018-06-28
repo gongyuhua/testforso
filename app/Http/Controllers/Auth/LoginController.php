fa3db928bea67d67c8caab915441e1cc11d9bf93
gongyuhua <yg348@njit.edu>
2018-06-27 20:14:36 -0400

    Procfile for Heroku

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -56,6 +56,6 @@ class LoginController extends Controller
     {
         $user = Socialite::driver('facebook')->stateless()->user();
 
-         return $user->name;
+         return $user->email;
     }
 }
